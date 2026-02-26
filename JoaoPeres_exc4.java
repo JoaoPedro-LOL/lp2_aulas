0e363ac60f4ec724295c1911679a1ce4da87bac0
joaoPedro_LOL <jpedroperesdasilva@gmail.com>
2026-02-26 09:01:53 -0300

    tst al 2

--- JoaoPeres_exc4.java
+++ JoaoPeres_exc4.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class JoaoPeres_exc4 {
-    
+
+    public static Scanner TECLADO = new Scanner(System.in);
+
+    public static int lerInt() {
+        return TECLADO.nextInt();
+    }
+
+    public static void lerMatrizInt(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = lerInt();
+            }
+        }
+    }
+
+    public static int somarLinhaColunaMatrizInt(int[][] matriz) {
+        int soma = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (i == 2 || j == 4) {
+                    soma += matriz[i][j];
+                }
+            }
+        }
+        return soma;
+    }
+
+    public static void main(String[] args) {
+        int[][] matriz = new int[7][6];
+        int soma;
+        lerMatrizInt(matriz);
+        soma = somarLinhaColunaMatrizInt(matriz);
+        System.out.printf("A soma da linha 3 e da coluna 5 da matriz é: %d\n", soma);
+    }
 }
